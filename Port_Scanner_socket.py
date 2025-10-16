7c8f5c23675381383debd73ea508d309efe07fef
Tarek Talab <tariktalab06@gmail.com>
2025-10-16 20:38:13 +0300

    Port_Scanner_socket.py

--- Port_Scanner_socket.py
+++ Port_Scanner_socket.py
@@ -7,7 +7,7 @@ colorama.init()
 banner = pyfiglet.figlet_format("Port Scanner")
 print(banner)
 
-giris = "Merhaba şimdi sizden IP address ile port aralığı girmenizi isticez ona göre bilgiler giriniz."
+giris = "Welcome! Please enter the target IP address and the port range to start the scan."
 for harf in giris:
     print(harf,end="",flush=True)
     time.sleep(0.02)
@@ -21,16 +21,16 @@ def port_scanner(ip_address,port_start,port_end):
         try:
             service_name = socket.getservbyport(port,"tcp")
         except OSError:
-            service_name = "Bilinmiyor"
-        if service_name == "Bilinmiyor":
+            service_name = "Unknown"
+        if service_name == "Unknown":
             print(f"Port No : {port} | Service: {service_name}")
         else:
             print(f"Port No: {colored(port,"red")} | Service: {colored(service_name,"red")}")
         sock.close()
         
 
-ip_address = input(colored("[+] IP:","red"))
-port_start = int(input(colored("[+] Port BaşlangıÇ sınırı: ","green")))
-port_end = int(input(colored("[+] Port bitiş sınırı: ","green")))
+ip_address = input(colored("[+] Enter Target IP Address: ","red"))
+port_start = int(input(colored("[+] Enter Starting Port Number: ","green")))
+port_end = int(input(colored("[+] Enter Ending Port Number: ","green")))
 port_scanner(ip_address,port_start,port_end)
 
